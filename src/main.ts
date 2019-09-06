ad30ac519d558ae1e35b71fd098850543266ee00
Warren Buckley <warren@umbraco.com>
2019-09-06 14:12:14 +0100

    Lets see if git commits work

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as fs from 'fs';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+
+    const find = core.getInput('find', { required: true });
+    const replace = core.getInput('replace', { required: true });
+
+    core.debug(`Scanning file names in checked out repo for '${find}'`);
+
+    // Rename file
+    fs.renameSync("README.md", "__README.md");
+
+    // Git status
+    await exec.exec("git", ["status"]);
+    await exec.exec("git", ["add", "."]);
+    await exec.exec("git", ["commit", "-m", "Can we commit?"]);
+    await exec.exec("git", ["push origin"]);
+
   } catch (error) {
     core.setFailed(error.message);
   }
